082a7b653a1142fc0b3058cfe7063f63b5f67bcd
Jessie <jess.bell@me.com>
2017-09-22 09:54:17 -0700

    Add js to display all numbers divisible by 3 as ping

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var emptyArray = [""]
 function pingPonger(input) {
   var inputNumber = parseInt(input)
   for (var i = inputNumber; i > 0; i = i-1) {
-    if (i === 3) {
+    if (i%3 === 0) {
       emptyArray.push("ping")
     }else {
     emptyArray.push(i);
