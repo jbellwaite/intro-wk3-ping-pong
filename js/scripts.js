1b46be8d3a15627c3e2a90888a4065259ba2e534
Jessie <jess.bell@me.com>
2017-09-22 10:35:50 -0700

    Add list items to each item within the array to display as an unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 //Back-End, Business Logic Section
 var emptyArray = [""]
 
+
 function pingPonger(input) {
   var inputNumber = parseInt(input)
   for (var i = inputNumber; i > 0; i = i-1) {
     if (i % 3 === 0) {
-      emptyArray.push("ping")
+      emptyArray.push("<li>" + "ping!" + "</li>")
     }else if (i % 5 === 0) {
-      emptyArray.push("pong")
+      emptyArray.push("<li>" + "pong!" + "</li>")
     }else if (i % 15 === 0) {
-      emptyArray.push("ping-pong")
+      emptyArray.push("<li>" + "ping-pong!" + "</li>")
     }else {
-    emptyArray.push(i);
+    emptyArray.push("<li>" + i + "</li>");
     }
   }
 }
@@ -25,6 +26,6 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $("#input").val();
     var variableSpot = pingPonger(input);
-    var output = $("#output").text(emptyArray.reverse())
+    var output = $("#output").append(emptyArray.reverse().join(" "));
   })
 })
