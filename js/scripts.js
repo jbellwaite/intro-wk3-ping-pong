9cc67e87b7aae30c3487e4c3f645722019b3a939
Jessie <jess.bell@me.com>
2017-09-22 13:35:56 -0700

    add more js to remove output from previous submission, continue testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //Back-End, Business Logic Section
-var emptyArray = [""]
+var emptyArray = [""];
 
 function pingPonger(input) {
   var inputNumber = parseInt(input)
   for (var i = inputNumber; i > 0; i = i - 1) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 15 != 0) {
       emptyArray.push("<li>" + "ping!" + "</li>")
-    }else if (i % 5 === 0) {
+    }else if (i % 5 === 0 && i % 15 != 0) {
       emptyArray.push("<li>" + "pong!" + "</li>")
     }else if (i % 15 === 0) {
       emptyArray.push("<li>" + "ping-pong!" + "</li>")
@@ -22,6 +22,9 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $("#input").val();
     var variableSpot = pingPonger(input);
-    $("ul#output").append(emptyArray.reverse().join(" "));
-  })
-})
+    $("ul#output").append(emptyArray.reverse());
+    $("ul#output").children("li").first().submit(function( {
+      $(this).remove();
+    }));
+  });
+});
